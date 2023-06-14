8b20a9dc10f3ed5b10b69c2171ab6821de39e09f
ESC18 <ponderosaprintingus@gmail.com>
2023-06-14 15:42:48 -0700

    Create additional functions and UI functionality.
    Co-Authored-by: <66178856+SmKou@users.noreply.github.com>
    
    Co-authored-by: Jake Elsberry <jelsberry96@gmail.com>

--- README.md
+++ README.md
@@ -10,43 +10,61 @@
 
 ## License
 
-<!-- Here are comments -->
+<!-- PROGRAM STRUCTURE -->
 
-Tests:
+-- game: Pig Dice
+- On a player's turn:
+    1. Roll a die => rollDie()
+    2. Add number from die to score => updateScore()
+      If number is 1, add nothing to score and switch to other player
+      If number is 2+, add number to score
+    3. Repeat step 1 or switch players
+    - End of turn: Check a player's score
+      If player's score is 100+, announce player as winner
+
+-- UI structure
+- player1: total score
+- player2: total score
+- die
+- roll button
+- hold button
+
+-- UI logic
+- Display die
+- Update player's total score
+- Announce winner
+
+-- game memory
 
 Controller
-- {String} active player
+Purpose: An object that keeps track of the game and players
+- {Player} active player
 - {Player} players
 - rollDie()
 - holdDie()
 
 Player
+Purpose: An object that keeps track of a player's current score and their roll history
 - {Number} score
-- {Number[]} score history
-OR
-let player1 = {
-    score: {number},
-    scoreHistory: {number[]}
-}
+
+<!-- TESTS -->
 
 Describe: rollDie()
+
+Purpse: Roll a die
+- generate a random number
+- return the random number
 
 Test: It will create a random number.
 Code: rollDie();
 Expected Output: A number between 1 and 6.
 
-Describe: turnLog()
+Describe: updateScore()
 
-Purpose: Create a history of user rolls
-- call variable to hold each roll result
-- use rollDie()
-- add result of rollDie() to variable
-- return variable
-
-- paper 
-- roll a die
-- write the number on the die on paper
-- report the new score 
+Purpose: Update score
+- get current score
+- add number from die to current score
+- report|return current score
 
 What if the array is [] => [*]
 What if the array has [1] => [1, *]
--- js/script.js
+++ js/script.js
@@ -1,4 +1,101 @@
 function rollDie() {
     let randomNumber = Math.floor(Math.random() * 6 + 1);
-    console.log(randomNumber);
+    return randomNumber;
+}
+
+function updateRunningTotal() {
+    let randomNumber = rollDie();
+    if (randomNumber !== 1)
+        controller.runningTotal += randomNumber;
+    else 
+        controller.runningTotal = 0;
+    return randomNumber;
+}
+
+function updateScore() {
+    controller.players[controller.activePlayer] += controller.runningTotal;
+    controller.runningTotal = 0;
+}
+
+function switchPlayer(player) {
+    if (player === 0)
+        return 1;
+    return 0;
+}
+
+const controller = {
+    init: function () {
+        this.activePlayer = 0;
+        this.players = [0, 0];
+        this.runningTotal = 0;
+    },
+    isThereWinner: function () {
+        if (this.players[this.activePlayer] >= 100)
+            return this.activePlayer;
+        this.activePlayer = switchPlayer(this.activePlayer);
+        return -1;
+    }
+}
+
+/* UI logic */
+
+function switchActivePlayer(player1, player2) {
+    if (player1.classList.contains('active')) {
+        player1.classList.remove('active');
+        player2.classList.add('active');
+    }
+    else {
+        player2.classList.remove('active');
+        player1.classList.add('active');
+    }
+}
+
+window.onload = () => {
+    const die = document.querySelector('.die');
+    const winner = document.querySelector('.winner');
+    const announcement = document.querySelector('.announcement');
+    const player1 = document.querySelector('.score-1');
+    const player2 = document.querySelector('.score-2');
+
+    const displayScores = function () {
+        player1.innerText = controller.players[0];
+        player2.innerText = controller.players[1];
+    }
+
+    const isEndGame = function () {
+        const winner = controller.isThereWinner();
+        if (winner > -1) {
+            winner.classList.remove('hidden');
+            announcement.innerText = 'The winner is player ' + (winner + 1);
+        }
+        else 
+            switchActivePlayer();
+        displayScores();
+    }
+
+    // Roll button
+    document.querySelector('#roll-btn').addEventListener('click', () => {
+        const n = updateRunningTotal();
+        die.innerText = n;
+        if (n === 1) {
+            updateScore();
+            switchActivePlayer();
+        }
+        displayScores();
+    });
+    
+    // Hold button
+    document.querySelector('#hold-btn').addEventListener('click', () => {
+        updateScore();
+        
+        
+    });
+
+    // Restart button
+    document.querySelector('#restart-btn').addEventListener('click', () => {
+        controller.init();
+        displayScores(player1, player2);
+        announcement.innerText = '';
+        winner.classList.add('hidden');
+    })
 }
